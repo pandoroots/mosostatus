2aea1608ebc8ef56c0a4eecab8ea7adab6428e3c
Mike Klein <mike@meshy.ca>
2013-06-01 11:18:50 -0600

    removed index

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,8 @@ Status::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   
+   root :to => 'venues#index'
 
   # See how all your routes lay out with "rake routes"
 
