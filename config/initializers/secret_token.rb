0f04c629aa0f34283e4fda992536c50878e7b3af
Mike Klein <mike@meshy.ca>
2013-06-01 10:24:44 -0600

    First commit, set up rails and created venue model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Status::Application.config.secret_token = '9056840b9cc70b8ab9a294195b475fe61b13f05be0872166cfa8878626a6d8afde0d42754420d20243602777357ab2e66779ab7f349a1b16e54bd411d3c8b455'
