53e0717fe579d514ec71bae1a156dcaf2016048d
Mike Klein <mike@meshy.ca>
2013-06-01 14:18:12 -0600

    Adding devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Status::Application.routes.draw do
+  devise_for :users
+
   resources :venues
+
+  root :to => 'venues#index'
+
 
 
   # The priority is based upon order of creation:
@@ -52,7 +57,6 @@ Status::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
    
-   root :to => 'venues#index'
 
   # See how all your routes lay out with "rake routes"
 
